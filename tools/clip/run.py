b820de5220d333fea237dea33a4e10cbac4facb5
Trey Moen <trey@moen.ai>
2025-04-30 15:49:14 -0700

    set constant fps, buffer input

--- tools/clip/run.py
+++ tools/clip/run.py
@@ -140,7 +140,7 @@ def clip(data_dir: str | None, quality: Literal['low', 'high'], prefix: str, rou
   display = f':{randint(99, 999)}'
 
   ffmpeg_cmd = [
-    'ffmpeg', '-y', '-video_size', RESOLUTION, '-framerate', str(FRAMERATE), '-f', 'x11grab', '-draw_mouse', '0',
+    'ffmpeg', '-y', '-r', str(FRAMERATE), '-vsync', 'cfr', '-re', '-video_size', RESOLUTION, '-framerate', str(FRAMERATE), '-f', 'x11grab', '-draw_mouse', '0',
     '-i', display, '-c:v', 'libx264', '-maxrate', f'{bit_rate_kbps}k', '-bufsize', f'{bit_rate_kbps*2}k', '-crf', '23',
     '-preset', 'ultrafast', '-pix_fmt', 'yuv420p', '-movflags', '+faststart', '-f', 'mp4', '-t', str(duration), output_filepath,
   ]
