3f7b797eafbfb01e1a4d2df1621e270efec30cbe
Trey Moen <trey@moen.ai>
2025-10-29 22:15:07 -0700

    back

--- system/ui/tests/screen_tearing_stress_test.py
+++ system/ui/tests/screen_tearing_stress_test.py
@@ -3,13 +3,13 @@
 import pyray as rl
 from openpilot.common.realtime import config_realtime_process
 from openpilot.system.ui.lib.application import gui_app
-from openpilot.selfdrive.ui.layouts.settings.settings import SettingsLayout
+from openpilot.selfdrive.ui.layouts.settings.firehose import FirehoseLayout
 
 def main():
     config_realtime_process([1, 2], 1)
 
     gui_app.init_window("Screen Tearing Stress Test")
-    shredder = SettingsLayout()
+    shredder = FirehoseLayout()
     shredder.set_rect(rl.Rectangle(0, 0, gui_app.width, gui_app.height))
 
     for should_render in gui_app.render():
