4f816c8b4a12c009b43ea4ef0907ed0ea808f797
Trey Moen <trey@moen.ai>
2025-10-29 22:27:55 -0700

    wifi?

--- system/ui/tests/screen_tearing_stress_test.py
+++ system/ui/tests/screen_tearing_stress_test.py
@@ -5,9 +5,14 @@ from openpilot.common.realtime import config_realtime_process
 from openpilot.system.ui.lib.application import gui_app
 from openpilot.selfdrive.ui.layouts.settings.firehose import FirehoseLayout
 from openpilot.selfdrive.ui.layouts.settings.settings import SettingsLayout
+from openpilot.system.ui.lib.wifi_manager import WifiManager
 
 def main():
     config_realtime_process([1, 2], 1)
+
+    wifi = WifiManager()
+    wifi.set_active(True)
+    wifi.add_callbacks(networks_updated=lambda networks: print(networks))
 
     gui_app.init_window("Screen Tearing Stress Test")
     shredder = FirehoseLayout()
