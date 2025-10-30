0cf2046c442db56bf33d9c0a6c788da52b8bcd5d
Trey Moen <trey@moen.ai>
2025-10-29 22:55:53 -0700

    back

--- system/ui/tests/screen_tearing_stress_test.py
+++ system/ui/tests/screen_tearing_stress_test.py
@@ -15,18 +15,17 @@ def main():
     wifi.add_callbacks(networks_updated=lambda networks: print(networks))
 
     gui_app.init_window("Screen Tearing Stress Test")
-    #shredder = FirehoseLayout()
-    shredder = SettingsLayout()
+    shredder = FirehoseLayout()
+    # shredder = SettingsLayout()
     rect = rl.Rectangle(0, 0, gui_app.width, gui_app.height)
-    # inset = rl.Rectangle(rect.x + 50, rect.y + 50, rect.width - 100, rect.height - 100)
-    panel_rect = rl.Rectangle(rect.x + 500, rect.y, rect.width - 500, rect.height)
+    panel_rect = rl.Rectangle(rect.x + 500, rect.y + 25, rect.width - 500, rect.height - 55)
     shredder.set_rect(panel_rect)
 
     for should_render in gui_app.render():
         if should_render:
-            # rl.draw_rectangle_rounded(
-            #     inset, 0.04, 30, rl.Color(24, 24, 24, 255)
-            # )
+            rl.draw_rectangle_rounded(
+                panel_rect, 0.04, 30, rl.Color(24, 24, 24, 255)
+            )
             shredder.render()
 
     gui_app.close()
